db557314ba013e93eda89400c471682b95655667
vee w <kawewong@gmail.com>
2018-05-31 12:42:34 +0700

    Update to Bootstrap 4.1.1

--- _php-doc/php-inc/inc_html_foot.php
+++ _php-doc/php-inc/inc_html_foot.php
@@ -1,7 +1,7 @@
 
 
         <script src="js/jquery-3.1.1.min.js"></script>
-        <script src="js/bootstrap.bundle.min.js"></script>
-        <script src="js/main.js"></script>
+        <script src="js/bootstrap.bundle.min.js?v=<?php echo date('Ymd'); ?>"></script>
+        <script src="js/main.js?v=<?php echo date('Ymd'); ?>"></script>
     </body>
 </html>
--- _php-doc/php-inc/inc_html_head.php
+++ _php-doc/php-inc/inc_html_head.php
@@ -7,8 +7,8 @@
         <meta name="description" content="Bootstrap 4 element styles and classes">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css?v=<?php echo date('Ymd'); ?>">
+        <link rel="stylesheet" href="css/main.css?v=<?php echo date('Ymd'); ?>">
     </head>
 
     <body>
